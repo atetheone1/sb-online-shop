e61c63522e555ffb56e8f6fadd30686dcd675beb
atetheone <atevirran@gmail.com>
2024-05-31 18:22:02 +0000

    Add constructors, setters and getters

--- src/main/java/tech/atetheone/sb_online_shop/domain/Customer.java
+++ src/main/java/tech/atetheone/sb_online_shop/domain/Customer.java
@@ -8,6 +8,47 @@ public class Customer {
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
 
-  private String name, city;
-  private int balance;
+  private String fullname, city;
+  private double balance;
+
+  public Customer() {
+  }
+
+  public Customer(String name, String city, double balance) {
+    this.fullname = name;
+    this.city = city;
+    this.balance = balance;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getFullname() {
+    return fullname;
+  }
+
+  public void setFullname(String name) {
+    this.fullname = name;
+  }
+
+  public String getCity() {
+    return city;
+  }
+
+  public void setCity(String city) {
+    this.city = city;
+  }
+
+  public double getBalance() {
+    return balance;
+  }
+
+  public void setBalance(double balance) {
+    this.balance = balance;
+  }
 }

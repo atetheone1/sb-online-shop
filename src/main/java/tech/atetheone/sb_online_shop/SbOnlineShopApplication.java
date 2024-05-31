a1ac106e274e7c14e1ca3febe94526af259abc68
atetheone <atevirran@gmail.com>
2024-05-31 18:11:56 +0000

    spring boot initialized

---
+++ src/main/java/tech/atetheone/sb_online_shop/SbOnlineShopApplication.java
@@ -0,0 +1,13 @@
+package tech.atetheone.sb_online_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbOnlineShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbOnlineShopApplication.class, args);
+	}
+
+}

64ec569180dbc1fe8047997319031b942f77942d
atetheone <atevirran@gmail.com>
2024-05-31 18:19:22 +0000

    Product class added

---
+++ src/main/java/tech/atetheone/sb_online_shop/domain/Product.java
@@ -0,0 +1,10 @@
+package tech.atetheone.sb_online_shop.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Product {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+}

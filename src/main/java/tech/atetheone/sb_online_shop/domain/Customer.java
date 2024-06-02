e423a74af3142491bfc924fb05e7d227bdfe14c9
atetheone <atevirran@gmail.com>
2024-06-02 15:25:29 +0000

    Update Customer class by adding the remaining attributes

--- src/main/java/tech/atetheone/sb_online_shop/domain/Customer.java
+++ src/main/java/tech/atetheone/sb_online_shop/domain/Customer.java
@@ -2,6 +2,7 @@ package tech.atetheone.sb_online_shop.domain;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Entity(name = "Customers")
@@ -9,9 +10,12 @@ public class Customer {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
-
-  private String fullname, city;
+  private String fullname;
+  private String password;
+  private String role;
   private double balance;
+  private LocalDate createdAt;
+  private LocalDate updatedAt;
 
   @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL, mappedBy = "customer")
   private List<Order> orders;
@@ -19,10 +23,13 @@ public class Customer {
   public Customer() {
   }
 
-  public Customer(String name, String city, double balance) {
-    this.fullname = name;
-    this.city = city;
+  public Customer(String fullname, String password, String role, double balance, LocalDate createdAt, LocalDate updatedAt) {
+    this.fullname = fullname;
+    this.password = password;
+    this.role = role;
     this.balance = balance;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
   }
 
   public Long getId() {
@@ -37,16 +44,24 @@ public class Customer {
     return fullname;
   }
 
-  public void setFullname(String name) {
-    this.fullname = name;
+  public void setFullname(String fullname) {
+    this.fullname = fullname;
   }
 
-  public String getCity() {
-    return city;
+  public String getPassword() {
+    return password;
   }
 
-  public void setCity(String city) {
-    this.city = city;
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getRole() {
+    return role;
+  }
+
+  public void setRole(String role) {
+    this.role = role;
   }
 
   public double getBalance() {
@@ -57,11 +72,41 @@ public class Customer {
     this.balance = balance;
   }
 
+  public LocalDate getCreatedAt() {
+    return createdAt;
+  }
+
+  public void setCreatedAt(LocalDate createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  public LocalDate getUpdatedAt() {
+    return updatedAt;
+  }
+
+  public void setUpdatedAt(LocalDate updatedAt) {
+    this.updatedAt = updatedAt;
+  }
+
   public List<Order> getOrders() {
     return orders;
   }
 
   public void setOrders(List<Order> orders) {
     this.orders = orders;
+  }
+
+  @Override
+  public String toString() {
+    return "Customer{" +
+            "id=" + id +
+            ", fullname='" + fullname + '\'' +
+            ", password='" + password + '\'' +
+            ", role='" + role + '\'' +
+            ", balance=" + balance +
+            ", createdAt=" + createdAt +
+            ", updatedAt=" + updatedAt +
+            ", orders=" + orders +
+            '}';
   }
 }

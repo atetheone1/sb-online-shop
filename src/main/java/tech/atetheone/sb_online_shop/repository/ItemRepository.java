dcc3ad329ce268f70d2b6ca08961236dae0b4131
atetheone <atevirran@gmail.com>
2024-06-02 15:49:58 +0000

    Add ItemRepository interface

---
+++ src/main/java/tech/atetheone/sb_online_shop/repository/ItemRepository.java
@@ -0,0 +1,9 @@
+package tech.atetheone.sb_online_shop.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import tech.atetheone.sb_online_shop.domain.Item;
+
+
+public interface ItemRepository extends CrudRepository<Item, Long> {
+
+}

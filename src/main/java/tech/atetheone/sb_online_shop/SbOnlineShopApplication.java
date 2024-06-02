d6017f5d178bb82dd122fe4b5677ee2ccbb04876
atetheone <atevirran@gmail.com>
2024-06-02 16:37:01 +0000

    Change the loop to print all the items for each order

--- src/main/java/tech/atetheone/sb_online_shop/SbOnlineShopApplication.java
+++ src/main/java/tech/atetheone/sb_online_shop/SbOnlineShopApplication.java
@@ -7,8 +7,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import tech.atetheone.sb_online_shop.domain.Customer;
 import tech.atetheone.sb_online_shop.domain.Order;
+import tech.atetheone.sb_online_shop.domain.Product;
+import tech.atetheone.sb_online_shop.domain.Item;
 import tech.atetheone.sb_online_shop.repository.CustomerRepository;
+import tech.atetheone.sb_online_shop.repository.ItemRepository;
 import tech.atetheone.sb_online_shop.repository.OrderRepository;
+import tech.atetheone.sb_online_shop.repository.ProductRepository;
 
 import java.util.Arrays;
 
@@ -17,10 +21,19 @@ public class SbOnlineShopApplication implements CommandLineRunner {
 	private static final Logger logger = LoggerFactory.getLogger(SbOnlineShopApplication.class);
 	private final CustomerRepository customerRepository;
 	private final OrderRepository orderRepository;
+  private final ItemRepository itemRepository;
+  private final ProductRepository productRepository;
 
-public SbOnlineShopApplication(CustomerRepository customerRepository, OrderRepository orderRepository) {
+public SbOnlineShopApplication(
+        CustomerRepository customerRepository,
+        OrderRepository orderRepository,
+        ItemRepository itemRepository,
+        ProductRepository productRepository
+) {
 		this.customerRepository = customerRepository;
 		this.orderRepository = orderRepository;
+    this.itemRepository = itemRepository;
+    this.productRepository = productRepository;
 	}
 
 	public static void main(String[] args) {
@@ -30,27 +43,41 @@ public SbOnlineShopApplication(CustomerRepository customerRepository, OrderRepos
 
 	@Override
 	public void run(String... args) throws Exception {
-		Customer c1 = new Customer("Bugs Bunny", "New York", 59000);
-		Customer c2 = new Customer("Daffy Duck", "Los Angeles", 37000);
-		Customer c3 = new Customer("Porky Pig", "Miami", 28000 );
-		customerRepository.saveAll(Arrays.asList(c1,c2, c3));
+    // Create some customers
+		Customer c1 = new Customer("Bugs Bunny", "New York", "user",  59000);
+		Customer c2 = new Customer("Daffy Duck", "Los Angeles", "user", 37000);
+		Customer c3 = new Customer("Porky Pig", "Miami", "user", 28000 );
+    customerRepository.saveAll(Arrays.asList(c1,c2, c3));
 
-		Order o1 = new Order(100, c1);
-		Order o2 = new Order(300, c1);
-		Order o3 = new Order(200, c2);
-		Order o4 = new Order(140, c3);
-		orderRepository.saveAll(Arrays.asList(o1, o2, o3, o4));
+    Order o1 = new Order(c1);
+		Order o2 = new Order(c1);
+		Order o3 = new Order(c2);
+		Order o4 = new Order(c3);
+    orderRepository.saveAll(Arrays.asList(o1, o2, o3, o4));
 
-		int i_order = 0;
+    // Create a product
+    Product p1 = new Product("Banana", "Yellow fruit", "banana.jpg", 200);
+    Product p2 = new Product("Apple", "Red fruit", "apple.jpg", 250);
+    productRepository.saveAll(Arrays.asList(p1, p2));
+
+    // Create an item
+    Item i1 = new Item(2, 200, o1, p1);
+    Item i2 = new Item(3, 300, o2, p2);
+
+    itemRepository.saveAll(Arrays.asList(i1, i2));
+
+
 		System.out.println("----- All Orders ------");
 
-		for (Order o : orderRepository.findAll()) {
-			i_order++;
+    for (Order o : orderRepository.findAll()) {
+      System.out.println("Order: " + o.getId() + ", Customer: " + o.getCustomer().getFullname() + ", Total: " + o.getTotal() + "$");
 
-			System.out.print("order_"+i_order+" \t "+o.getCustomer().getFullname()+"\t "+o.getTotal()+"$");
+      for (Item item : o.getItems()) {
+        System.out.println("\tItem: " + item.getProduct().getName() + ", Quantity: " + item.getQuantity() + ", Price: " + item.getPrice());
+      }
 
-			System.out.println(" ");
-		}
+      System.out.println("-----             ------");
+    }
 		System.out.println("-----             ------");
 
 	}

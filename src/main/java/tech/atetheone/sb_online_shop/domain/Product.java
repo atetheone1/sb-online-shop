3306cb4df94395bfa7eb66bd46da6ba2edbd9e9e
atetheone <atevirran@gmail.com>
2024-06-02 15:39:33 +0000

    Update Product class

--- src/main/java/tech/atetheone/sb_online_shop/domain/Product.java
+++ src/main/java/tech/atetheone/sb_online_shop/domain/Product.java
@@ -1,10 +1,98 @@
 package tech.atetheone.sb_online_shop.domain;
 
 import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 public class Product {
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
+
+  private String name;
+  private String description;
+  private String image;
+  private int price;
+  private LocalDateTime createdAt;
+  private LocalDateTime updatedAt;
+
+  @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+  private List<Item> items;
+
+  public Product() {
+  }
+
+  public Product(String name, String description, String image, int price, LocalDateTime createdAt, LocalDateTime updatedAt) {
+    this.name = name;
+    this.description = description;
+    this.image = image;
+    this.price = price;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public String getImage() {
+    return image;
+  }
+
+  public void setImage(String image) {
+    this.image = image;
+  }
+
+  public int getPrice() {
+    return price;
+  }
+
+  public void setPrice(int price) {
+    this.price = price;
+  }
+
+  public LocalDateTime getCreatedAt() {
+    return createdAt;
+  }
+
+  public void setCreatedAt(LocalDateTime createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  public LocalDateTime getUpdatedAt() {
+    return updatedAt;
+  }
+
+  public void setUpdatedAt(LocalDateTime updatedAt) {
+    this.updatedAt = updatedAt;
+  }
+
+  public List<Item> getItems() {
+    return items;
+  }
+
+  public void setItems(List<Item> items) {
+    this.items = items;
+  }
 }

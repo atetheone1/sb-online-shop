262e18056422e6538fd830c005f26676757f3992
atetheone <atevirran@gmail.com>
2024-06-02 17:49:48 +0000

    Print the orders

--- src/main/java/tech/atetheone/sb_online_shop/SbOnlineShopApplication.java
+++ src/main/java/tech/atetheone/sb_online_shop/SbOnlineShopApplication.java
@@ -56,28 +56,35 @@ public SbOnlineShopApplication(
     orderRepository.saveAll(Arrays.asList(o1, o2, o3, o4));
 
     // Create a product
-    Product p1 = new Product("Banana", "Yellow fruit", "banana.jpg", 200);
-    Product p2 = new Product("Apple", "Red fruit", "apple.jpg", 250);
-    productRepository.saveAll(Arrays.asList(p1, p2));
+		Product shirt = new Product("Shirt", "Nice shirt", "shirt.jpg", 200);
+		Product phone = new Product("Phone", "Smartphone", "phone.jpg", 200);
+		Product tv = new Product("TV", "Smart TV", "tv.jpg", 300);
+		Product shoes = new Product("Shoes", "Running shoes", "shoes.jpg", 300);
+    productRepository.saveAll(Arrays.asList(shirt, phone, tv, shoes));
 
     // Create an item
-    Item i1 = new Item(2, 200, o1, p1);
-    Item i2 = new Item(3, 300, o2, p2);
+		// Create items and assign to orders
+		Item i1 = new Item(1, shirt.getPrice(), o1, shirt);
+		Item i2 = new Item(1, phone.getPrice(), o2, phone);
+		Item i3 = new Item(1, tv.getPrice(), o2, tv);
+		Item i4 = new Item(1, shoes.getPrice(), o2, shoes);
+		Item i5 = new Item(1, tv.getPrice(), o3, tv);
+		Item i6 = new Item(1, phone.getPrice(), o3, phone);
+		Item i7 = new Item(1, shoes.getPrice(), o3, shoes);
+		Item i8 = new Item(1, tv.getPrice(), o4, tv);
+		Item i9 = new Item(1, shirt.getPrice(), o4, shirt);
+		itemRepository.saveAll(Arrays.asList(i1, i2, i3, i4, i5, i6, i7, i8, i9));
 
-    itemRepository.saveAll(Arrays.asList(i1, i2));
 
 
+		// Print all orders
 		System.out.println("----- All Orders ------");
-
-    for (Order o : orderRepository.findAll()) {
-      System.out.println("Order: " + o.getId() + ", Customer: " + o.getCustomer().getFullname() + ", Total: " + o.getTotal() + "$");
-
-      for (Item item : o.getItems()) {
-        System.out.println("\tItem: " + item.getProduct().getName() + ", Quantity: " + item.getQuantity() + ", Price: " + item.getPrice());
-      }
-
-      System.out.println("-----             ------");
-    }
+		for (Order o : orderRepository.findAll()) {
+			System.out.println("Customer: " + o.getCustomer().getFullname() + ": Order " + o.getId() + ": Total: " + o.getTotal() + "$");
+			for (Item item : o.getItems()) {
+				System.out.println("\tItem: " + item.getQuantity() + " x " + item.getProduct().getName() + " (" + item.getProduct().getPrice() + "$)");
+			}
+		}
 		System.out.println("-----             ------");
 
 	}

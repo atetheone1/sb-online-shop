979b6ab617c0bcfb264b95c68dd64ba8eb41ca13
atetheone <atevirran@gmail.com>
2024-06-02 15:39:11 +0000

    Update id attribute

--- src/main/java/tech/atetheone/sb_online_shop/domain/Item.java
+++ src/main/java/tech/atetheone/sb_online_shop/domain/Item.java
@@ -6,7 +6,7 @@ import java.time.LocalDateTime;
 @Entity
 public class Item {
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private int id;
 
   private int quantity;

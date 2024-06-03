a026192b7156bc0101475d98ed6f818c55fc8b56
atetheone <atevirran@gmail.com>
2024-06-03 07:57:48 +0000

    Refactor by implementing the builder pattern

--- src/main/java/tech/atetheone/sb_online_shop/SbOnlineShopApplication.java
+++ src/main/java/tech/atetheone/sb_online_shop/SbOnlineShopApplication.java
@@ -15,6 +15,7 @@ import tech.atetheone.sb_online_shop.repository.OrderRepository;
 import tech.atetheone.sb_online_shop.repository.ProductRepository;
 
 import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class SbOnlineShopApplication implements CommandLineRunner {
@@ -43,39 +44,137 @@ public SbOnlineShopApplication(
 
 	@Override
 	public void run(String... args) throws Exception {
-    // Create some customers
-		Customer c1 = new Customer("Bugs Bunny", "New York", "user",  59000);
-		Customer c2 = new Customer("Daffy Duck", "Los Angeles", "user", 37000);
-		Customer c3 = new Customer("Porky Pig", "Miami", "user", 28000 );
-    customerRepository.saveAll(Arrays.asList(c1,c2, c3));
+		// Create some customers
+		List<Customer> customers = Arrays.asList(
+						Customer.builder().fullname("Bugs Bunny").password("New York").role("user").balance(59000).build(),
+						Customer.builder().fullname("Daffy Duck").password("Los Angeles").role("user").balance(37000).build(),
+						Customer.builder().fullname("Porky Pig").password("Miami").role("user").balance(28000).build()
+		);
+		/*Customer c1 = Customer.builder()
+						.fullname("Bugs Bunny")
+						.password("New York")
+						.role("user")
+						.balance(59000)
+						.build();
 
-    Order o1 = new Order(c1);
-		Order o2 = new Order(c1);
-		Order o3 = new Order(c2);
-		Order o4 = new Order(c3);
-    orderRepository.saveAll(Arrays.asList(o1, o2, o3, o4));
+		Customer c2 = Customer.builder()
+						.fullname("Daffy Duck")
+						.password("Los Angeles")
+						.role("user")
+						.balance(37000)
+						.build();
 
-    // Create a product
-		Product shirt = new Product("Shirt", "Nice shirt", "shirt.jpg", 200);
-		Product phone = new Product("Phone", "Smartphone", "phone.jpg", 200);
-		Product tv = new Product("TV", "Smart TV", "tv.jpg", 300);
-		Product shoes = new Product("Shoes", "Running shoes", "shoes.jpg", 300);
-    productRepository.saveAll(Arrays.asList(shirt, phone, tv, shoes));
+		Customer c3 = Customer.builder()
+						.fullname("Porky Pig")
+						.password("Miami")
+						.role("user")
+						.balance(28000)
+						.build();*/
 
-    // Create an item
-		// Create items and assign to orders
-		Item i1 = new Item(2, shirt.getPrice(), o1, shirt);
-		Item i2 = new Item(1, phone.getPrice(), o2, phone);
-		Item i3 = new Item(1, tv.getPrice(), o2, tv);
-		Item i4 = new Item(1, shoes.getPrice(), o2, shoes);
-		Item i5 = new Item(1, tv.getPrice(), o3, tv);
-		Item i6 = new Item(1, phone.getPrice(), o3, phone);
-		Item i7 = new Item(1, shoes.getPrice(), o3, shoes);
-		Item i8 = new Item(1, tv.getPrice(), o4, tv);
-		Item i9 = new Item(1, shirt.getPrice(), o4, shirt);
-		itemRepository.saveAll(Arrays.asList(i1, i2, i3, i4, i5, i6, i7, i8, i9));
+		customerRepository.saveAll(customers);
+		//customerRepository.saveAll(Arrays.asList(c1, c2, c3));
+
+		// Create some orders
+		List<Order> orders = Arrays.asList(
+						Order.builder().customer(customers.get(0)).build(),
+						Order.builder().customer(customers.get(0)).build(),
+						Order.builder().customer(customers.get(1)).build(),
+						Order.builder().customer(customers.get(2)).build()
+		);
 
 
+		orderRepository.saveAll(orders);
+
+		// Create a product
+		Product shirt = Product.builder()
+						.name("Shirt")
+						.description("Nice shirt")
+						.image("shirt.jpg")
+						.price(200)
+						.build();
+
+		Product phone = Product.builder()
+						.name("Phone")
+						.description("Smartphone")
+						.image("phone.jpg")
+						.price(200)
+						.build();
+
+		Product tv = Product.builder()
+						.name("TV")
+						.description("Smart TV")
+						.image("tv.jpg")
+						.price(300)
+						.build();
+
+		Product shoes = Product.builder()
+						.name("Shoes")
+						.description("Running shoes")
+						.image("shoes.jpg")
+						.price(300)
+						.build();
+
+		productRepository.saveAll(Arrays.asList(shirt, phone, tv, shoes));
+
+
+		// Create items
+		List<Item> items = Arrays.asList(
+						Item.builder()
+										.quantity(2)
+										.price(shirt.getPrice())
+										.order(orders.get(0))
+										.product(shirt)
+										.build(),
+						Item.builder()
+										.quantity(1)
+										.price(phone.getPrice())
+										.order(orders.get(1))
+										.product(phone)
+										.build(),
+						Item.builder()
+										.quantity(1)
+										.price(tv.getPrice())
+										.order(orders.get(1))
+										.product(tv)
+										.build(),
+						Item.builder()
+										.quantity(1)
+										.price(shoes.getPrice())
+										.order(orders.get(1))
+										.product(shoes)
+										.build(),
+						Item.builder()
+										.quantity(1)
+										.price(tv.getPrice())
+										.order(orders.get(2))
+										.product(tv)
+										.build(),
+						Item.builder()
+										.quantity(1)
+										.price(phone.getPrice())
+										.order(orders.get(2))
+										.product(phone)
+										.build(),
+						Item.builder()
+										.quantity(1)
+										.price(shoes.getPrice())
+										.order(orders.get(2))
+										.product(shoes)
+										.build(),
+						Item.builder()
+										.quantity(1)
+										.price(tv.getPrice())
+										.order(orders.get(3))
+										.product(tv)
+										.build(),
+						Item.builder()
+										.quantity(1)
+										.price(shirt.getPrice())
+										.order(orders.get(3))
+										.product(shirt)
+										.build()
+		);
+		itemRepository.saveAll(items);
 
 		// Print all orders
 		System.out.println("----- All Orders ------");
@@ -86,6 +185,5 @@ public SbOnlineShopApplication(
 			}
 		}
 		System.out.println("-----             ------");
-
 	}
 }

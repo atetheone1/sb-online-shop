cfbbbe2ec855e11f43f203161d92756a4ccbe07c
atetheone <atevirran@gmail.com>
2024-06-06 23:52:36 +0000

    Update OrderRepository

--- src/main/java/tech/atetheone/sb_online_shop/repository/OrderRepository.java
+++ src/main/java/tech/atetheone/sb_online_shop/repository/OrderRepository.java
@@ -1,6 +1,7 @@
 package tech.atetheone.sb_online_shop.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import tech.atetheone.sb_online_shop.domain.Order;
 
 public interface OrderRepository extends CrudRepository<Order, Long> {

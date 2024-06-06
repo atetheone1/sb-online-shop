821ff4e7ec9a8466d1d3feb70d06ace7196a539e
atetheone <atevirran@gmail.com>
2024-06-06 23:53:33 +0000

    Update ItemRepository

--- src/main/java/tech/atetheone/sb_online_shop/repository/ItemRepository.java
+++ src/main/java/tech/atetheone/sb_online_shop/repository/ItemRepository.java
@@ -1,8 +1,8 @@
 package tech.atetheone.sb_online_shop.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import tech.atetheone.sb_online_shop.domain.Item;
-
 
 public interface ItemRepository extends CrudRepository<Item, Long> {
 

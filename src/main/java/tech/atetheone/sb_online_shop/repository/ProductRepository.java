4fc87a1feb5d5b87a8c6a72dc3a6592ef9c88322
atetheone <atevirran@gmail.com>
2024-06-08 22:36:31 +0000

    Update the repository dir

--- src/main/java/tech/atetheone/sb_online_shop/repository/ProductRepository.java
+++ src/main/java/tech/atetheone/sb_online_shop/repository/ProductRepository.java
@@ -1,7 +1,6 @@
 package tech.atetheone.sb_online_shop.repository;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import tech.atetheone.sb_online_shop.domain.Product;
 
 public interface ProductRepository extends CrudRepository<Product, Long> {
